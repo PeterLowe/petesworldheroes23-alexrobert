a34c27f296c05180c334415d7fcb5325d2f8e87d
(Student  C00297358) Robert Iovu <C00297358@setu.ie>
2023-11-10 12:17:33 +0000

    PetesWorld

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -78,6 +78,52 @@ int main()
 			std::cout << "there are no Bs in the rest of the world" << std::endl;
 		}
 	}
+	if (letter == 'w' || letter == 'W')
+	{
+		if (region == 1)
+		{
+			std::cout << "There are no Ws in the rest of the world" << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "There are no Ws in America" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "There are no Ws in Europe" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There are no Ws in Asia" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "Wallis and Futuna Islands, Western Sahara" << std::endl;
+		}
+	}
+	if (letter == 'y' || letter == 'Y')
+	{
+		if (region == 1)
+		{
+			std::cout << "There is no Ys in America" << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "There are no Ys in Europe" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "Yemen Arabian Peninsula" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There are no Ys in Asia" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "There are no Ys in rest of the world" << std::endl;
+		}
+	}
 	if (letter == 'z' || letter == 'Z') // written by Pete
 	{
 		if (region == 1)
@@ -101,29 +147,7 @@ int main()
 			std::cout << "There are no Zs in rest of the world" << std::endl;
 		}
 
-	}
-	if (letter == 'y' || letter == 'Y')
-	{
-		if (region == 1)
-		{
-			std::cout << "There is no Ys in America" << std::endl;
-		}
-		if (region == 2)
-		{
-			std::cout << "There are no Ys in Europe" << std::endl;
-		}
-		if (region == 3)
-		{ 
-			std::cout << "Yemen Arabian Peninsula" << std::endl;
-		}
-		if (region == 4)
-		{
-			std::cout << "There are no Ys in Asia" << std::endl;
-		}
-		if (region == 5)
-		{
-			std::cout << "There are no Ys in rest of the world" << std::endl;
-		}
+	
 	}
 	
 	return 1;
